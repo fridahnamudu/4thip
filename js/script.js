b8582012dfa24f4472f3c61556a0af97f5492894
fridahnamudu <fridah.namudu@gmail.com>
2020-08-03 00:22:04 +0300

    class checkbox to the file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-alert("hello!");
+$(document).ready(function(){
+    $("#ok").click(function(){
+        alert("Your pizza will be delivered at :" +$("#location").val());
+    });
+
+    $("#submit").click(function(){
+        alert("message has been recieved thank you for shopping with us!")
+    });
+});
