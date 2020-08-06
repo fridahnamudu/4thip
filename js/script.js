3a39faa594076cf4b7a916be2d5dff0cf2928be3
fridahnamudu <67759660+fridahnamudu@users.noreply.github.com>
2020-08-06 09:30:22 +0300

    dlelete script.js

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $("#ok").click(function(){
-        alert("Your pizza will be delivered at :" +$("#location").val());
-    });
-
-    $("#submit").click(function(){
-        alert("message has been recieved thank you for shopping with us!")
-    });
-});
